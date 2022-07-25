d7f10f13850786e64fbc63c7fa19604f9a8b200b
diegokieck <diegokieck@gmail.com>
2022-07-25 10:48:24 +0000

    added result to utils

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import pickle
+import pandas as pd 
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 def plot_trial_results(trials):
   exp_list =[]
   for trial in trials.trials:
@@ -25,3 +28,13 @@ def load_trial(path):
   with open(path, 'rb') as f:
     trials=pickle.load(f)
   return trials
+
+  def compute_results(y_true, y_pred ):
+  accuracy = accuracy_score(y_true, y_pred)
+  f1_macro = f1_score(y_true, y_pred, average='macro')
+  recall_macro = recall_score(y_true, y_pred, average='macro')
+  precision_macro = precision_score(y_true, y_pred, average='macro')
+  return {'accuracy': accuracy,
+          'f1_macro': f1_macro,
+          'recall_macro': recall_macro,
+          'precision_macro': precision_macro}
