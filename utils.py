98181df85ceaf0d34bf6d0bf51d0a14c64f3e66d
diegokieck <diegokieck@gmail.com>
2022-07-30 23:16:52 +0000

    added word cnn results

--- utils.py
+++ utils.py
@@ -39,7 +39,28 @@ def compute_results(y_true, y_pred ):
           'recall_macro': recall_macro,
           'precision_macro': precision_macro}
 
-  
+  import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+#função para visualizar o historico de treinamento da rede
+def plot_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
 
 
 
